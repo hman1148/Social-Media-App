813238cceff8803380d33aecf5165e5de6fc28d5
Hunter Peart <hman1148@outlook.com>
2023-02-01 12:51:19 -0700

    Converted all module imports from EMS to CommonJS, still having issue with declaration of 'register line 54 of index.js

--- server/index.js
+++ server/index.js
@@ -15,11 +15,16 @@
 // const {register} = require("./controllers/auth.js");
 
 import { fileURLToPath } from "url";
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
 
-
-
-
+import path from "path";
 
 const filename = fileURLToPath(import.meta.url);
 
