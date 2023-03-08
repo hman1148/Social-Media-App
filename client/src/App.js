b1a7dc757c5fc846806d2a87d2149fb25f76607b
Hunter Peart <hman1148@outlook.com>
2023-03-08 16:08:07 -0700

    Fixed the majority of the compliling error bugs, still having issues on the dom but am moving forward with project and will resolve issues from there

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,22 @@ import HomePage from './scenes/homepage';
 import LoginPage from './scenes/loginpage';
 import ProfilePage from './scenes/profilepage';
 import { useMemo } from 'react';
-import {userSelector} from 'react-redux';
-import { CssBaseLine, ThemeProvider} from "@mui/material";
-import {createTheme} from "@mui/material/styles";
+import {useSelector} from 'react-redux';
+import { CssBaseline, ThemeProvider } from "@mui/material";import {createTheme} from "@mui/material/styles";
 import { themeSettings } from './theme.';
 
 
 
 function App() {
-  const mode = userSelector((state) => state.mode);
-  const theme = userMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
 
   return (
     <div className="App">
     <BrowserRouter>
     <ThemeProvider theme={theme}>
-      <CssBaseLine /> 
+      <CssBaseline /> 
       <Routes>
         <Route path = "/" element={<LoginPage />} />
         <Route path = "/home" element={<HomePage />} />
--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import authReducer from "./state";
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
-import persistReducer from 'redux-persist/es/persistReducer';
-
-const persistConfig = { key: "root", storage, version: 1};
-const persistReducer = persistReducer(persistConfig, authReducer);
-const store = configureStore({ // docs from redux persist
-  reducer: persistReducer,
-  middleware: (getDefaultMiddleware) => 
-  getDefaultMiddleware({ 
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE,PAUSE,PERSIST, PURGE,REGISTER ]
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-  }),
+    }),
 });
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
